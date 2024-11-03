5dd813b9092ff503a0bd562df2020ed4f1758dd4
albnzhum <albnzhum@gmail.com>
2024-11-03 16:19:36 +0500

    Added pz4

---
+++ pz4/DataBase.cs
@@ -0,0 +1,19 @@
+﻿namespace pz4;
+
+public class DataBase
+{
+    public void SaveData(string text)
+    {
+        // Блокировка метода, чтобы он был доступен только одному потоку в данный момент
+        lock (this)
+        {
+            Console.WriteLine($"Начинается сохранение данных: {text}");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Сохранение данных {text}... шаг {i + 1}");
+                Thread.Sleep(500);
+            }
+            Console.WriteLine($"Завершено сохранение данных: {text}");
+        }
+    }
+}
---
+++ pz4/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace pz4;
+
+class Program
+{
+    public static DataBase db = new DataBase();
+
+    public static void WorkerThreadMethod1()
+    {
+        Console.WriteLine("Поток 1 стартовал");
+        db.SaveData("Данные из потока 1");
+        Console.WriteLine("Поток 1 завершён");
+    }
+
+    public static void WorkerThreadMethod2()
+    {
+        Console.WriteLine("Поток 2 стартовал");
+        db.SaveData("Данные из потока 2");
+        Console.WriteLine("Поток 2 завершён");
+    }
+
+    static void Main(string[] args)
+    {
+        ThreadStart worker1 = new ThreadStart(WorkerThreadMethod1);
+        ThreadStart worker2 = new ThreadStart(WorkerThreadMethod2);
+
+        Thread t1 = new Thread(worker1);
+        Thread t2 = new Thread(worker2);
+
+        t1.Start();
+        t2.Start();
+
+        t1.Join();
+        t2.Join();
+
+        Console.WriteLine("Все потоки завершены.");
+    }
+}
