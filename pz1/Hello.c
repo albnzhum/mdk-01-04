292cb3c795493b79a3fac793ed13e94a9c70fabf
albnzhum <albnzhum@gmail.com>
2024-11-03 15:05:43 +0500

    Added pz1

---
+++ pz1/Hello.c
@@ -0,0 +1,46 @@
+﻿#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    setlocale(LC_ALL, "Russian"); // установка русской локали
+    int size;
+    printf("Введите размер массива: ");
+    fflush(stdout); // очистка выходного буфера
+    scanf_s("%d", &size);
+    int* array = malloc(size * sizeof(*array)); // выделение блока памяти для массива
+
+    int i = 0;
+    printf("Введите элементы массива: ");
+    fflush(stdout);
+
+    while (i < size)
+    {
+        scanf_s("%d", &array[i]); // ввод элементов массива
+        i++;
+    }
+    // сортировка
+    for (int i = 1; i < size; i++)
+    {
+        const int key = array[i];
+        int j = i - 1;
+
+        while (j >= 0 && array[j] > key)
+        {
+            array[j + 1] = array[j];
+            j--;
+        }
+
+        array[j + 1] = key;
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+        fflush(stdout);
+    }
+
+    free(array); // освобождение памяти
+    return 0;
+}
