5cf54f03ff1d33ada8ae073b1326d7f015b63eee
albnzhum <albnzhum@gmail.com>
2024-11-03 15:11:57 +0500

    Added pz2

--- pz2/Program.cs
+++ pz2/Program.cs
@@ -1,9 +1,37 @@
-﻿namespace pz2;
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace pz2;
 
 class Program
 {
+    [DllImport("user32.dll")]
+    public static extern int GetSystemMetrics(int nIndex);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+    private static extern int GetComputerName(StringBuilder nameBuffer, ref int bufferSize);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern uint GetVersion();
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        StringBuilder nameBuffer = new StringBuilder(256); // Буфер для имени компьютера
+        int bufferSize = nameBuffer.Capacity; // Размер буфера
+
+        GetComputerName(nameBuffer, ref bufferSize);
+        Console.WriteLine("Имя компьютера: " + nameBuffer.ToString());
+
+        uint version = GetVersion();
+        uint majorVersion = Convert.ToUInt32(version & 0x000000FF);
+        uint minorVersion = Convert.ToUInt32((version & 0x0000FF00) >> 8);
+        Console.WriteLine("Версия ОС: " + majorVersion + "." + minorVersion);
+
+        uint build = 0;
+        if (version < 0x80000000) // Если версия ОС больше, чем Windows NT
+        {
+            build = Convert.ToUInt32((version & 0xFFFF0000) >> 16);
+            Console.WriteLine("Сборка ОС: " + build);
+        }
     }
 }
